8ed5960bdefbff0cff3e3e669f4ba3b4694ab8d2
huzhengbiao <hans.hu@livestar.com>
2017-09-26 16:58:36 +0800

    将应用加入到播放视频文件的可选方式中
                <intent-filter>
                    <action android:name="android.intent.action.VIEW"/>
                    <category android:name="android.intent.category.DEFAULT"/>
    
                    <data android:mimeType="video/*"></data>
                </intent-filter>

---
+++ app/src/main/java/com/cdut/hzb/iplayerdemo/IntentKey.java
@@ -0,0 +1,9 @@
+package com.cdut.hzb.iplayerdemo;
+
+/**
+ * Created by hans on 2017/9/26 0026.
+ */
+
+class IntentKey {
+    public static final String VIDEO_PATH = "video_path";
+}
---
+++ app/src/main/java/com/cdut/hzb/iplayerdemo/VideoPlayerActivity.java
@@ -0,0 +1,129 @@
+package com.cdut.hzb.iplayerdemo;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Surface;
+import android.view.TextureView;
+
+import java.io.IOException;
+
+public class VideoPlayerActivity extends AppCompatActivity {
+
+    private TextureView mTextureView;
+    private MediaPlayer mediaPlayer;
+    private Surface mSurface;
+
+    private String videoPath;
+
+    public static void start(Context context, String videoPath) {
+        Intent intent = new Intent(context, VideoPlayerActivity.class);
+//        intent.putExtra(IntentKey.VIDEO_PATH, videoPath);
+        intent.setData(Uri.parse(videoPath));
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player);
+        requestPermission();
+        getData();
+        mTextureView = (TextureView) findViewById(R.id.texture_view);
+        mTextureView.setSurfaceTextureListener(listener);
+
+        mediaPlayer = new MediaPlayer();
+    }
+
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        getData();
+    }
+
+    // 可以通过Intent.setData方法设置数据源
+
+    public void getData() {
+        if (getIntent().getData() != null) {
+            videoPath = getIntent().getData().getPath();
+        }
+    }
+
+    private TextureView.SurfaceTextureListener listener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            mSurface = new Surface(surface);
+            mediaPlayer.setSurface(mSurface);
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mediaPlayer.start();
+                }
+            });
+            try {
+                if (!TextUtils.isEmpty(videoPath)) {
+                    mediaPlayer.setDataSource(videoPath);
+                    mediaPlayer.prepare();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            if (mediaPlayer != null) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                mediaPlayer = null;
+                mSurface = null;
+            }
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
